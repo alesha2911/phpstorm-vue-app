ad2eb7704f1bbc9db53066f11ebb3bc2ed1ddfc2
Alex Prokopenko <alex2911@dimti.ru>
2022-07-14 19:03:41 +0300

    + color h3 - color a

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -69,6 +69,7 @@ export default {
 <style scoped>
 h3 {
   margin: 40px 0 0;
+  color: #42b983;
 }
 ul {
   list-style-type: none;
@@ -77,8 +78,5 @@ ul {
 li {
   display: inline-block;
   margin: 0 10px;
-}
-a {
-  color: #42b983;
 }
 </style>
